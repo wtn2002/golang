157ff5cdf5d70e9b173d9e4ce112e27cdab868af
wtn2002 <wtn2002@gmail.com>
2017-01-21 13:42:27 +0700

    Update Go2

--- src/Test/Test2.go
+++ src/Test/Test2.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 func main() {
 	fmt.Println("test test2")
-<<<<<<< HEAD
 	fmt.Println("test 2 test2\n")
-=======
 	fmt.Println("test 3")
->>>>>>> 2eb8cab9b5a290e2c9ffd63560ad339c4bcbe7f5
 }
